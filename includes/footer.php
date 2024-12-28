a15ad9879fc4a3040e92744b164dea9f2d76777a
dxMausam7 <bajimayamausam@gmail.com>
2024-12-28 14:33:55 +0545

    Footer Added

---
+++ includes/footer.php
@@ -0,0 +1,9 @@
+<footer class="py-3 mt-4">
+    <div class="container-fluid d-flex justify-content-center align-items-center">
+        <p class="large m-0 text-center text-lg-start text-dark">
+            &copy; 2024 Strumo All Rights Reserved - DX_20779
+        </p>
+
+
+    </div>
+</footer>
